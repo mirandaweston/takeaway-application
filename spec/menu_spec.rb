ae69d7c262f4113d3a106b3a313943a9330e8c9a
mirandaweston <mweston105@yahoo.co.uk>
2023-01-29 18:05:08 +0000

    Updating Menu class test suite following changes to Menu class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Menu do
     subject { described_class.new }
     
     it "displays the menu to the customer" do
-        expect { subject.display_menu }.to output("Menu\n-------\nPizza: 5\nPasta: 7\nLasagne: 8\n").to_stdout
+        expect { subject.display_menu }.to output("       Menu        \n******************\nPizza: 5\nPasta: 7\nLasagne: 8\n").to_stdout
     end
 end
